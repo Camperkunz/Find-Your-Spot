83f219f17431f3ad3f5c675e28d1f91cdb5207a3
Camperkunz <anyanikiforova04@gmail.com>
2026-07-16 23:28:09 -0400

    feature (styles): new header

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,13 +1,11 @@
-import { useAppContext } from '../../context/AppContext';
-
 export default function Header() {
-    const { step, setStep, savedPlaces } = useAppContext();
-
     return (
-        <header>
-            <h1>
-                Header
-            </h1>
+        <header className="flex h-20 items-center justify-between px-6 md:px-16 border-b border-surface-muted">
+            <button onClick={() => setStep('welcome')} className="flex items-center hover:cursor-pointer">
+                <h1 className="text-xl md:text-xl font-bold text-accent">
+                    Where should I go?
+                </h1>
+            </button>
         </header>
     );
 }
