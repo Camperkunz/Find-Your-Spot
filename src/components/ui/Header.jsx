fb1d281b2ead58cc4b5ef30855da3b8176ec671e
Camperkunz <anyanikiforova04@gmail.com>
2026-07-22 21:31:22 -0400

    feat: updated header, added saved places counter

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,11 +1,23 @@
+import { useAppContext } from '../../context/AppContext';
+
 export default function Header() {
+    const { step, setStep, savedPlaces } = useAppContext();
+    const hasSaved = savedPlaces.length > 0;
+    // 
     return (
-        <header className="flex h-20 items-center justify-between px-6 md:px-16 border-b border-surface-muted">
+        <header className="flex h-20 items-center justify-between px-6 md:px-16 border-b border-surface-muted bg-surface">
             <button onClick={() => setStep('welcome')} className="flex items-center hover:cursor-pointer">
                 <h1 className="text-xl md:text-xl font-bold text-accent">
                     Where should I go?
                 </h1>
             </button>
+            {hasSaved && (
+                <button
+                    className="btn-pill border border-surface-muted/50 "
+                >
+                    Saved ({savedPlaces.length})
+                </button>
+            )}
         </header>
     );
 }
